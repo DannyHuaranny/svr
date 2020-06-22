9ab1862bc3e965e5f1600285a7297d27310bea5d
DannyHuaranny <58090793+DannyHuaranny@users.noreply.github.com>
2020-06-21 20:36:39 -0500

    Update IndexController.php
    ca

--- Controller/IndexController.php
+++ Controller/IndexController.php
@@ -20,57 +20,6 @@ public function View(){
 require_once("Views/index.php");
 }
 
-public function Create(){
-    
-    
-    require_once("Views/Index/create.php");
-    }
-
-    public function Update(){
-        // recibir el id enviado en el formulario
-
-       $id=$_REQUEST['id'];
-       // enviar id al modelo
-            $datos=$this->model->ver_editar($id);      
-        require_once("Views/Index/update.php");                      
-        }
-
-    public function Crear(){
-
-        $Indeximiento = new IndexModel();
-        
-        $Indeximiento->nom_mov = $_REQUEST['nom_mov'];
-         $Indeximiento->Index = $_REQUEST['Index'];
-       
-   
-        $this->model->crear($Indeximiento);
-        //Redireccionar
-        header('Location: ?c=Index&a=View');
-        }
-
-        public function Eliminar(){
-
-            $idcliente=$_REQUEST['id'];
-            $this->model->eliminar($idcliente);
-           
-            //Redireccionar
-            header('Location: ?c=Index&a=View');
-            }
-
-         
-
-            public function Editar(){
-
-                $Indeximiento = new IndexModel();
-        
-                $Indeximiento->nom_mov = $_REQUEST['nom_mov'];
-                $Indeximiento->Index = $_REQUEST['Index'];
-                $Indeximiento->id = $_REQUEST['id'];
-                $this->model->editar($Indeximiento);
-                //Redireccionar
-                header('Location: ?c=Index&a=View');
-                               
-                }
 
               
 
