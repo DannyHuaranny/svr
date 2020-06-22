57ba7ba846a8a4a9a9e5907a3afa89cecf350f5e
DannyHuaranny <58090793+DannyHuaranny@users.noreply.github.com>
2020-06-21 20:52:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,231 @@
+<?php include('Layouts/header.php')?>
 
-<?php
-require_once 'Controller/IndexController.php';
+    <!-- slider-area-start -->
+    <div class="slider-area">
+        <div class="slider-active owl-carousel">
+            <div class="single-slider bg-img-1" style="background-image: url(Layouts/imagenes/fotos/fondo-1.jpg)">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-7 offset-xl-1 col-lg-7">
+                            <div class="slider-content">
+                                <p>Trabajo de calidad. Servicio confiable. Equipo dedicado</p>
+                                <h3>Brindamos su solución industrial</h3>
+                                <div class="slider-btn">
+                                    <a class="boxed-btn2" href="#">Nuestros Servicios<i
+                                            class="Flaticon flaticon-right-arrow"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-slider bg-img-1" style="background-image: url(Layouts/imagenes/fotos/fondo-2.jpg)">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-7 offset-xl-1 col-lg-7" >
+                            <div class="slider-content">
+                                <p>Trabajo de calidad. Servicio confiable. Equipo dedicado</p>
+                                <h3>Brindamos su solución industrial</h3>
+                                <div class="slider-btn">
+                                    <a class="boxed-btn2" href="#">Nuestros Servicios<i
+                                            class="Flaticon flaticon-right-arrow"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-//Primera parte - Nombre Controlador
-$controller = 'Index';
+        </div>
+    </div>
+   
+    <!-- service-area-start -->
+    <div class="service-area" style="margin-top: -100px !important;">
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div class="section-title text-center mb-65">
+                    <span>Nuestros Servicios</span>
+                    <h3>Brindamos toda su <br>
+                        solución industrial</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-4 col-md-4">
+                    <div class="single-service">
+                        <div class="service-thumb">
+                            <img src="Layouts/imagenes/DESINFECCION/1.jpg" alt="" style="height: 300px !important">
+                        </div>
+                        <h3>Limpieza, Lavado y Desinfección de Bienes </h3>
+                        <p>Garantizamos un trabajo con estandares de calidad y seguridad.</p>
+                        <a href="#" class="read-more">Leer Mas</a>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-4">
+                    <div class="single-service">
+                        <div class="service-thumb">
+                            <img src="Layouts/imagenes/PUBLICIDAD/1.JPG" alt="" style="height: 300px !important">
+                        </div>
+                        <h3>Publicidad e Imagen Exterior</h3>
+                        <p>Fabricamos todo tipo de Publicidad Exterior, diseños, montaje e instalacion.<b>
+Gigantografías,  Totems de grifos, Paneles exteriores, Letreros luminosos.</b></p>
+                        <a href="#" class="read-more">Leer Mas</a>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-4">
+                    <div class="single-service">
+                        <div class="service-thumb">
+                            <img src="Layouts/imagenes/PINTURA/1.jpg" alt="" style="height: 300px !important">
+                        </div>
+                        <h3>Pintura en General</h3>
+                        <p>Trabajos diversos en señalización, Horizontal y Vertical.
 
-// Todo esta lógica hara el papel de un FrontController
-if(!isset($_REQUEST['c']))
-{
-    require_once "Controller/$controller"."Controller.php";
-    $controller = $controller . 'Controller';
-    $controller = new $controller;
-    $controller->View();    
-}
-else
-{
-    // Obtenemos el controlador que queremos cargar
-    $controller = ucfirst($_REQUEST['c']);
-    $accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'View';
+Pinturas tipo tráfico para marcas sobre pavimento. (carreteras, calles, avenidas, muros de contención, etc.)</p>
+                        <a href="#" class="read-more">Leer Mas</a>
+                    </div>
+                </div>
+<div class="row" style="margin-top:10px;">
+                <div class="col-xl-4 col-md-4">
+                    <div class="single-service">
+                        <div class="service-thumb">
+                            <img src="Layouts/imagenes/ESTRUCTURAS/1.jpg" alt="" style="height: 300px !important">
+                        </div>
+                        <h3>Estructuras Metálicas</h3>
+                        <p>Realizamos fabricación y montaje de techos CANOPI, Mantenimiento de totem publicitario, <b>Desmontaje de Totems y techos, todo tipo de estructuras metálicas</b></p>
+                        <a href="#" class="read-more">Leer Mas</a>
+                    </div>
+                </div>
+
+
+                <div class="col-xl-4 col-md-4">
+                    <div class="single-service">
+                        <div class="service-thumb">
+                            <img src="Layouts/imagenes/ICONOS/mas.jpg" alt="" style="height: 300px !important">
+                        </div>
+                        <h3>Proximamente</h3>
+                        <p></p>
+                        <a href="#" class="read-more">Leer Mas</a>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-md-4">
+                    <div class="single-service">
+                        <div class="service-thumb">
+                            <img src="Layouts/imagenes/ICONOS/mas.jpg" alt="" style="height: 300px !important">
+                        </div>
+                        <h3>proximamente</h3>
+                        <p></p>
+                        <a href="#" class="read-more">Leer Mas</a>
+                    </div>
+                </div>
+
+</div>
+            </div>
+        </div>
+    </div>
+    <!-- service-area-end -->
+
+    <!-- project-area-start -->
+    <div class="project-area bg-img-2 overlay" style="background-image: url(Layouts/imagenes/fotos/fondo-2.jpg)">
+        <div class="container-fluid p-lg-0">
+            <div class="row justify-content-end no-gutters">
+                <div class="col-xl-4 col-md-6">
+                    <div class="section-title text-white mb-65 ml-80">
+                        <h3>Echa un vistazo a <br>
+                            nuestros proyectos</h3>
+                        <p>Realizamos proyectos integrales de Imagen Corporativa, basándose en control de calidad en:
+
+ <br> Estaciones de servicios, Instituciones, Locales comerciales, complejos turísticos y supermercados.</p>
+                        <!--<div class="more-project" >
+                            <a href="#" >ver más proyectos</a>
+                        </div>-->
+                    </div>
+                </div>
+                <div class="col-xl-6 col-md-6">
+                    <div class="project-active owl-carousel">
+                        <div class="single-project">
+                            <div class="project-thumb">
+                                <img src="Layouts/imagenes/PUBLICIDAD/1.jpg" alt="">
+                            </div>
+                            <div class="project-info">
+                                <span>Montaje e Instalación de Publicitarios</span>
+                                <h3>Vista de obra culminada</h3>
+                            </div>
+                        </div>
+                        <div class="single-project">
+                            <div class="project-thumb">
+                                <img src="Layouts/imagenes/PINTURA/1.jpg" alt="">
+                            </div>
+                            <div class="project-info">
+                                 <span>Pintura General en estacion de servicios</span>
+                                <h3>Vista de obra culminada</h3>
+                            </div>
+                        </div>
+                        <div class="single-project">
+                            <div class="project-thumb">
+                                <img src="Layouts/imagenes/ESTRUCTURAS/1.jpg" alt="">
+                            </div>
+                            <div class="project-info">
+                                 <span>Contruccion e instalación de estructura </span>
+                                <h3>Vista de obra culminada</h3>
+                            </div>
+                        </div>
+                      
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- project-area-end -->
+
+
+    <!-- about end -->
+
+
+    <!-- location-area-start -->
+    <div class="addres-area black-bg section-padding" style="background-color:black !important">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-4 col-md-4">
+                    <div class="single-address text-center">
+                        <div class="addres-icon">
+                            <img src="Layouts/img/icon/1.png" alt="">
+                        </div>
+                        <h3>Nuestra Dirección</h3>
+                        <p> Mz D Lote 10  Asociacion Los Sauces II - San juan de Lurigancho- Lima <br>
+                            Lima - Perú</p>
+                        <a class="underline-hover" href="#">Obtener Dirección</a>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-4">
+                    <div class="single-address text-center">
+                        <div class="addres-icon">
+                            <img src="Layouts/img/icon/2.png" alt="">
+                        </div>
+                        <h3>Horario de atención</h3>
+                        <p>Lunes - Viernes (8.00-5.30) <br>
+                            Sábados<a class="underline-hover" href="#">(8.00-12.30)</a></p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-4">
+                    <div class="single-address text-center">
+                        <div class="addres-icon">
+                            <img src="Layouts/img/icon/3.png" alt="">
+                        </div>
+                        <h3>Envíanos un mensaje</h3>
+                        <p>evilchezs.svr@gmail.com <br>
+                        980-488-407 / 958-078-143</p>
+                        <a class="underline-hover" href="#">E-Mail</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- location-area-end -->
+    <?php include('Layouts/footer.php')?>
+
+    <script>
     
-    // Instanciamos el controlador
-    require_once "Controller/$controller"."Controller.php";
-    $controller = $controller . 'Controller';
-    $controller = new $controller;
-    
-    // Llama la accion
-    call_user_func( array( $controller, $accion ) );
-}
-?>
+    $('#inicio').addClass('active');
+// $('.home').removeClass('animated rubberBand');
+
+    </script>
