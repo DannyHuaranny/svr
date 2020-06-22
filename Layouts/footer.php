e5c2a89b4cee6fffa8be6fe659310c9f56aa3e8f
DannyHuaranny <58090793+DannyHuaranny@users.noreply.github.com>
2020-06-21 20:16:54 -0500

    asd
    asd

---
+++ Layouts/footer.php
@@ -0,0 +1,49 @@
+
+    <!-- footer-start -->
+    <footer class="footer-area ">
+      
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-lg-12">
+                    <div class="copyright_part_text text-center">
+                        <p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- footer-end -->
+
+    <!-- JS here -->
+    <script src="Layouts/js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="Layouts/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="Layouts/js/popper.min.js"></script>
+    <script src="Layouts/js/bootstrap.min.js"></script>
+    <script src="Layouts/js/owl.carousel.min.js"></script>
+    <script src="Layouts/js/isotope.pkgd.min.js"></script>
+    <script src="Layouts/js/ajax-form.js"></script>
+    <script src="Layouts/js/waypoints.min.js"></script>
+    <script src="Layouts/js/jquery.counterup.min.js"></script>
+    <script src="Layouts/js/imagesloaded.pkgd.min.js"></script>
+    <script src="Layouts/js/scrollIt.js"></script>
+    <script src="Layouts/js/jquery.scrollUp.min.js"></script>
+    <script src="Layouts/js/wow.min.js"></script>
+    <script src="Layouts/js/nice-select.min.js"></script>
+    <script src="Layouts/js/jquery.slicknav.min.js"></script>
+    <script src="Layouts/js/jquery.magnific-popup.min.js"></script>
+    <script src="Layouts/js/plugins.js"></script>
+
+    <!--contact js-->
+    <script src="Layouts/js/contact.js"></script>
+    <script src="Layouts/js/jquery.ajaxchimp.min.js"></script>
+    <script src="Layouts/js/jquery.form.js"></script>
+    <script src="Layouts/js/jquery.validate.min.js"></script>
+    <script src="Layouts/js/mail-script.js"></script>
+
+    <script src="Layouts/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ Layouts/header.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html class="no-js" lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>SVR | Servicios Generales S.A.C</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- <link rel="manifest" href="site.webmanifest"> -->
+    <link rel="shortcut icon" type="image/x-icon" href="Layouts/img/favicon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="Layouts/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Layouts/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="Layouts/css/magnific-popup.css">
+    <link rel="stylesheet" href="Layouts/css/font-awesome.min.css">
+    <link rel="stylesheet" href="Layouts/css/themify-icons.css">
+    <link rel="stylesheet" href="Layouts/css/nice-select.css">
+    <link rel="stylesheet" href="Layouts/css/flaticon.css">
+    <link rel="stylesheet" href="Layouts/css/animate.css">
+    <link rel="stylesheet" href="Layouts/css/slicknav.css">
+    <link rel="stylesheet" href="Layouts/css/style.css">
+    <link rel="stylesheet" href="Layouts/css/index.css">
+    <!-- <link rel="stylesheet" href="css/responsive.css"> -->
+</head>
+
+<body>
+    <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+    <!-- header-start -->
+    <header>
+        <div class="header-area ">
+            <div class="header-top black-bg d-none d-md-block" >
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-8 col-md-8 col-lg-8">
+                            <div class="header-contact">
+                                <a href="#"><i class="fa fa-whatsapp"></i>980-488-407 / 958-078-1432</a>
+                                <a href="#"><i class="fa fa-envelope"></i>evilchezs.svr@gmail.com</a>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-md-4 col-lg-4">
+                            <div class="header-top-menu">
+                                <nav>
+                                    <ul>
+                                        <li><a href="#">SVR Servicios Generales S.A.C.</a></li>
+                                       
+                                      
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="sticky-header" class="main-header-area white-bg" style="background-color:#399625eb; ">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-2 col-lg-2">
+                            <div class="logo-img" >
+                                <a href="./">
+                                    <img style="border-radius:10px;" id="logo-sticky" src="Layouts/imagenes/svrs-2.JPG" alt="" width="100px"  height="60px" style=" ">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-7 col-lg-7">
+                            <div class="main-menu d-none d-lg-block">
+                                <nav>
+                                    <ul id="navigation" >
+                                        <li><a id="inicio" href="./"><i class="fa fa-home"></i>&nbsp;Inicio</a></li>
+                                        <li><a id="nosotros" href="#">Nosotros</a></li>
+                                        <li><a href="#">Servicios</a></li>
+                                        <li><a href="#">Proyectos</a></li>
+                                        
+                                        <li><a href="#">Contacto</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-3">
+                            <div class="quote-area">
+                               
+                                <div class="get-quote d-none d-lg-block">
+                                    <a class="boxed-btn" href="#" style="border-radius: 10px;">Cotiza</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                  
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header-end -->
